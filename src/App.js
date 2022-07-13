6a8edb147592bb44303de3a77ed8c90c826d1c72
core.pager=cat <mohamedali.marouane7@gmail.com>
2022-07-13 14:00:38 +0200

    reduc checkpoint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import  ListTask from './componement/ListTask';
+import React, { Component } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <ListTask/>
+    </div>
+  );
+}
+
+export default App;
